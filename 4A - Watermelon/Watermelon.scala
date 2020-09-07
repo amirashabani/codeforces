c49cc5f916017d5c1e459f90c3acbb8567bb972d
Amir A. Shabani <amirshabani011@gmail.com>
2020-09-07 16:33:40 +0000

    Solution to problem 4A in Scala

---
+++ 4A - Watermelon/Watermelon.scala
@@ -0,0 +1,12 @@
+import scala.io.StdIn.readInt
+
+object Watermelon {
+    def main(args: Array[String]): Unit = {
+        val w = readInt()
+        if (w % 2 == 0 && w > 2) {
+            println("YES")
+        } else {
+            println("NO")
+        }
+    }
+}
