96afbd26fdddd0ad777623ae50e1bf18edffd89f
Amir A. Shabani <amirshabani011@gmail.com>
2020-09-10 18:41:16 +0000

    No need to continue after 1 has been found.

--- 263A - Beautiful Matrix/BeautifulMatrix.py
+++ 263A - Beautiful Matrix/BeautifulMatrix.py
@@ -8,6 +8,7 @@ for i in range(1, 6):
     if "1" in row:
         one_row = i
         one_column = row.index("1") + 1
+        break
 
 minimum_moves = abs(one_row - 3) + abs(one_column - 3)
 print(minimum_moves)
